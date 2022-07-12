2c894132f7d02f326dd5d145f029a28e3470a582
Ankit Bisen <ankitbisen288@gmail.com>
2022-07-12 09:31:07 +0530

    first Commit

---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Transaction } from "./Transaction";
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  return (
+    <>
+      <h2>History</h2>
+
+      <ul id="list" className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction}/>
+        ))}
+      </ul>
+    </>
+  );
+};
---
+++ src/context/GlobalState.js
@@ -0,0 +1,37 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from './AppReducer';
+
+// Intial state
+const intialState = {
+    transactions: []
+}
+
+// Create Context
+export const GlobalContext = createContext(intialState);
+
+// provider Component 
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, intialState);
+
+    //Actions 
+    function deleteTransaction(id) {
+        dispatch({
+            type: "DELETE_TRANSACTION",
+            payload: id,
+        });
+    }
+    
+    function addTransaction(transaction) {
+        dispatch({
+            type: "ADD_TRANSACTION",
+            payload: transaction,
+        });
+    }
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
